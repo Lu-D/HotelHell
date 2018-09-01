59bd6b7b5afc83b78e5c0af02153afa8f4aec03f
Don Choi <dongha.choi@vanderbilt.edu>
2018-08-31 20:55:21 -0500

    folder update

---
+++ Assets/Scripts/Attraction/BAttraction.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BAttraction : MonoBehaviour {
+
+    private int energySubtraction;
+    private int moneyEarned;
+    private int capacity;
+    private float timeSpentIn;
+    private int costToBuild;
+    private Collider2D myCollider;
+
+	// Use this for initialization
+	void Start () {
+        //myCollider = gameObject.transform.GetChild(0).GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<Renderer>().enabled = false;
+        }
+    }
+
+    public int getEnergySubtraction()
+    {
+        return energySubtraction;
+    }
+
+    public int getMoneyEarned()
+    {
+        return moneyEarned;
+    }
+
+    public int getCapacity()
+    {
+        return capacity;
+    }
+
+    public float getTimeSpentIn()
+    {
+        return timeSpentIn;
+    }
+
+    public int getCostToBuild()
+    {
+        return costToBuild;
+    }
+}
