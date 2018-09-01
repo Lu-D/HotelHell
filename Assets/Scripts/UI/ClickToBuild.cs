361f8cec83c7748147180ccfde99ea76ad206373
Don Choi <dongha.choi@vanderbilt.edu>
2018-09-01 10:18:24 -0500

    scene update

--- Assets/Scripts/UI/ClickToBuild.cs
+++ Assets/Scripts/UI/ClickToBuild.cs
@@ -6,22 +6,29 @@ public class ClickToBuild : MonoBehaviour {
 
     public GameObject[] buildings;
     public bool validBuild;
+    public bool isBuilding;
 
 	// Use this for initialization
 	void Start () {
         validBuild = false;
+        isBuilding = false;
 	}
-	
+
+    public void buildSelect() {
+        isBuilding = true;
+    }
+
 	// Update is called once per frame
 	void Update () {
 
         Debug.Log(validBuild);
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && isBuilding)
         {
 
             if (validBuild) {
                 Instantiate(buildings[0], Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)), buildings[0].transform.rotation);
+                isBuilding = false;
             }
         }
 	}
