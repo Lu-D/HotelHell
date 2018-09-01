7c6779ed078db0471d2f2d364047f5b6759fb933
Daiwei Lu <daiwei.lu@vanderbilt.edu>
2018-08-31 20:46:14 -0500

    Waypoint Sprites

---
+++ Assets/Scripts/EnemyControlScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControlScript : MonoBehaviour {
+    public float movespeed;
+    public int energy;
+    public GameObject[] notAffected;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
