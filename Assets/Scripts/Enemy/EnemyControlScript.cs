b8dc2e9c4854356682c7e23da98289596fb5f434
Daiwei Lu <daiwei.lu@vanderbilt.edu>
2018-09-01 00:49:45 -0500

    fixed Attraction

--- Assets/Scripts/Enemy/EnemyControlScript.cs
+++ Assets/Scripts/Enemy/EnemyControlScript.cs
@@ -8,20 +8,20 @@ public class EnemyControlScript : MonoBehaviour
     public int energy;
     public GameObject[] notAffected;
     public GameObject[] wayPoints;
-    public bool isCaptured;
-    public bool isLeaving;
+    private bool isCaptured;
     private int nextWayPoint;
 
     // Use this for initialization
     void Start()
     {
         nextWayPoint = 0;
-        isCaptured = isLeaving = false;
+        isCaptured = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(isCaptured);
     }
 
     public IEnumerator moveTowardsNext()
@@ -35,12 +35,48 @@ public class EnemyControlScript : MonoBehaviour
         }
     }
 
+    public IEnumerator moveTowardsAtractor(Transform attractor)
+    {
+        while (Vector3.Distance(transform.position, attractor.position) > 0.5)
+        {
+            //transform.GetComponent<Rigidbody2D>().velocity = ((attractor.position - transform.position).normalized * moveSpeed);
+            transform.position = Vector3.MoveTowards(transform.position, attractor.position, moveSpeed*Time.deltaTime);
+            yield return null;
+
+        }
+    }
+
+    public IEnumerator derez(float timeSpentIn)
+    {
+        transform.GetComponent<Rigidbody2D>().velocity = new Vector3(0,0,0);
+        gameObject.GetComponent<Renderer>().enabled = false;
+        yield return new WaitForSeconds(timeSpentIn);
+        gameObject.GetComponent<Renderer>().enabled = true;
+        isCaptured = false;
+        StartCoroutine(moveTowardsNext());
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.transform.tag == "Waypoint")
+
+        if (other.transform.tag == "Attraction Collider")
+        {
+            GameObject Attractor = other.transform.parent.gameObject;
+            if(Attractor.GetComponent<BAttraction>().currCapacity < Attractor.GetComponent<BAttraction>().maxCapacity)
+            {
+                isCaptured = true;
+                StartCoroutine(moveTowardsAtractor(Attractor.transform));
+            } 
+        }
+        if(other.transform.tag == "Attraction")
+        {
+            StartCoroutine(derez(other.transform.gameObject.GetComponent<BAttraction>().timeSpentIn));
+        }
+        if (other.transform.tag == "Waypoint")
         {
             ++nextWayPoint;
+            StartCoroutine(moveTowardsNext());
         }
-        StartCoroutine(moveTowardsNext());
+
     }
 }
