3feda2cff6202b82755191ab8e99c3e395a4ff08
Don Choi <dongha.choi@vanderbilt.edu>
2018-09-23 15:56:56 -0500

    documentation

--- Assets/Scripts/UI/ClickToBuild.cs
+++ Assets/Scripts/UI/ClickToBuild.cs
@@ -32,22 +32,24 @@ public class ClickToBuild : MonoBehaviour {
 	}
 
     //buildSelect
-    //sets state variable to true
+    //Changes bool when corresponding message is received
     public void buildSelect() {
         isBuilding = true;
     }
 
-    public void test()
-    {
-        Debug.Log("working");
-    }
-
+    //testAttract
+    //sets current building to be built as described by string
+    //turns on textures on map that can be built on
+    //only executes when message is received from button press
     public void testAttract(string building)
     {
         buildType = building;
         buildTextures.SetActive(true);
     }
 
+    //setCursor
+    //changes texture to corresponding texture about to be built
+    //only executes when message is received from buttone press
     public void setCursor(int buildArrayIndx)
     {
         buildArray = buildArrayIndx;
@@ -56,20 +58,25 @@ public class ClickToBuild : MonoBehaviour {
         Cursor.SetCursor(buildings[buildArrayIndx].GetComponent<BAttraction>().textures[0], cursorHotspot, cursorMode);     
     }
 
+    //build
+    //builds building if all parameters are met
     void build(string currBuilding, int buildArrayIndx)
     {
+        //if input is received, player is currently building, and player has enough money
         if (Input.GetMouseButtonDown(0) && isBuilding && buildType == currBuilding && buildings[buildArrayIndx].GetComponent<BAttraction>().costToBuild <= currMoney)
         {
-
+            //if the ground is a valid place to build
             if (validBuild)
             {
                 GameObject building = (GameObject)Instantiate(buildings[buildArrayIndx], Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)), buildings[buildArrayIndx].transform.rotation);
 
+                //dont allow disney castle to rotate
                 if (currBuilding != "DisneyCastle")
                 {
                     building.transform.Rotate(0, 0, rotationDegrees, Space.Self);
                 }
 
+                //reset all fields back to default
                 Cursor.SetCursor(null, Vector2.zero, cursorMode);
                 isBuilding = false;
                 buildType = "";
@@ -84,6 +91,7 @@ public class ClickToBuild : MonoBehaviour {
     // Update is called once per frame
     void Update () {
 
+        //speeds up game if t is pressed
         if (Input.GetKeyDown("t"))
         {
             Debug.Log("works1");
@@ -102,28 +110,32 @@ public class ClickToBuild : MonoBehaviour {
 
         }
 
+        //if player is building but the ground is not a valid build, indicate that build is invalid
         if(isBuilding && !validBuild)
         {
             cursorHotspot = new Vector2(buildings[buildArray].GetComponent<SpriteRenderer>().sprite.texture.width / 2, buildings[buildArray].GetComponent<SpriteRenderer>().sprite.texture.height / 2);
             Cursor.SetCursor(buildings[buildArray].GetComponent<BAttraction>().invalidTextures[currMouseTexture], cursorHotspot, cursorMode);
         }
+        //if player is building and the ground is a valid build, set cursor texture
         else if(isBuilding && validBuild)
         {
             Cursor.SetCursor(buildings[buildArray].GetComponent<BAttraction>().textures[currMouseTexture], cursorHotspot, cursorMode);
         }
 
-
+        //rotate building if space bar is pressed
         if (isBuilding && Input.GetKeyDown("space"))
         {
             rotationDegrees += 90;
             Debug.Log(rotationDegrees);
             Debug.Log("key pressed");
 
+            //reset rotation if it reaches 360
             if(rotationDegrees > 360)
             {
                 rotationDegrees -= 360;
             }
 
+            //change textures and building orientation at each degrees
             if (rotationDegrees == 90)
             {
                 buildings[buildArray].GetComponent<SpriteRenderer>().sprite = buildings[buildArray].GetComponent<BAttraction>().sprites[1];
@@ -156,6 +168,7 @@ public class ClickToBuild : MonoBehaviour {
 
         }
 
+        //builds game objects if parameters are met
         build("GameStop", 0);
         build("Hooters", 1);
         build("Supreme", 2);
@@ -163,8 +176,10 @@ public class ClickToBuild : MonoBehaviour {
         build("AMC", 4);
         build("DisneyCastle", 5);
 
+        //sets moeny counter
         moneyText.text = "Current money: " + currMoney;
 
+        //cancels builds and resets defaults
         if (Input.GetMouseButton(1))
         {
             Cursor.SetCursor(null, Vector2.zero, cursorMode);
